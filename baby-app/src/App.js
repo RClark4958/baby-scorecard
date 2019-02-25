0ff5f90f0e00d283874115b25bca9fa65851698d
RCLARK4958 <rickdclark48@gmail.com>
2019-02-25 10:30:11 -0700

    table

---
+++ baby-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TableComponent from './table'
+
+class App extends Component {
+  render() {
+    return (
+      
+      <TableComponent />
+
+    );
+  }
+}
+
+export default App;
